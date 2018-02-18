b809f5dac02f1a8819130929f8d708bd10757ccc
Ned Rihine <ned.rihine@gmail.com>
2018-02-18 13:05:56 +0900

    名前が長すぎるので、ソリューションとプロジェクトで名前を分けた
    - メインウィンドウの構成をちょっとだけ進捗

---
+++ PorkMisoSoup/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace TransferCenturyPorkMisoSoup.Views {
+
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
